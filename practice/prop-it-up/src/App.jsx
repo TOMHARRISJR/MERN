ceb6576d0d1daa2a9e0225174a0d83c208dfb07d
TOMHARRISJR <harristome@gmail.com>
2022-08-31 14:12:07 -0700

    caor

--- practice/prop-it-up/src/App.jsx
+++ practice/prop-it-up/src/App.jsx
@@ -5,10 +5,10 @@ import PersonalCard from './components/PersonalCard'
 function App() {
   return (
     <div className="App">
-      <PersonalCard name = {"Doe,Jane"} age = {45} hairColor = {"Black"}></PersonalCard>
-      <PersonalCard name = {"Smith,John"} age = {88} hairColor = {"Brown"}></PersonalCard>
-      <PersonalCard name = {"Fillmore,Millard"} age = {50} hairColor = {"Brown"}></PersonalCard>
-      <PersonalCard name = {"Smith,Maria"} age = {62} hairColor = {"Brown"}></PersonalCard>
+      <PersonalCard lastname = {"Doe"} firstname ={"Jane"} age = {45} hairColor = {"Black"}></PersonalCard>
+      <PersonalCard lastname = {"Smith"} firstname ={"John"} age = {88} hairColor = {"Brown"}></PersonalCard>
+      <PersonalCard lastname = {"Fillmore"} firstname ={"Millard"} age = {50} hairColor = {"Brown"}></PersonalCard>
+      <PersonalCard lastname = {"Smith"} firstname ={"Maria"} age = {62} hairColor = {"Brown"}></PersonalCard>
       
     </div>
   );
--- practice/prop-it-up/src/components/PersonalCard.jsx
+++ practice/prop-it-up/src/components/PersonalCard.jsx
@@ -1,12 +1,19 @@
 import React,{ Component } from 'react'
 
 class PersonalCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
     render(){
         return(
             <div className='item'>
-                <h1>{this.props.name}</h1>
-                <h3>Age: {this.props.age}</h3>
+                <h1>{this.props.lastname}, {this.props.firstname}</h1>
+                <h3>Age: {this.state.age}</h3>
                 <h3>Hair Color: {this.props.hairColor}</h3>
+            <button onClick={()=>this.setState({age:this.state.age+1})}>Birthday Button for {this.props.firstname} {this.props.lastname}</button>
             </div>
         )
     }
